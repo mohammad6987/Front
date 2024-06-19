12e4b836881e320fd17b32c44d6b4fb316aa2252
Mohammad <zare.6984@gmail.com>
2024-06-19 23:03:54 +0330

    adding routes to main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -2,9 +2,20 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
+import Home from './pages/home.jsx'
+import Login from './pages/login.jsx'
+import { RouterProvider, createBrowserRouter } from 'react-router-dom'
 
+const router = createBrowserRouter(
+  [
+    {path : '/', element :  <Home/> },
+    {path : '/login', element :  <Login/> },
+    
+  ]
+);
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
+    <RouterProvider router={router}/>
     <App />
   </React.StrictMode>,
 )
